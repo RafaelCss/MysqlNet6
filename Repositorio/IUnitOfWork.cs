97f1ac5a579297aae0ec61f3126a4c75f0241366
devspacerj <rf2014souza@gmail.com>
2023-03-30 07:15:39 -0300

    first commit

---
+++ Repositorio/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿
+using Dominio.Interface.Repositorio;
+
+namespace Dominio.Interface
+{
+    public interface IUnitOfWork
+	{
+		void BeginTransaction();
+		Task<int> Commit();
+		void Rollback();
+		IRepositorio<TEntity> Repositorio<TEntity>() where TEntity : class;
+	}
+
+}
+
